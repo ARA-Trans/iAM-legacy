35154a46ff801c84e1002cacc8b7f2a45e9ed932
Gregg Larson <glarson@ara.com>
2016-08-24 16:09:02 -0500

    Change to 3.71

--- RoadCare3/FormLogin.Designer.cs
+++ RoadCare3/FormLogin.Designer.cs
@@ -1110,7 +1110,7 @@
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(156, 13);
             this.label7.TabIndex = 4;
-            this.label7.Text = "Version 3.70    Date:8/24/2016";
+            this.label7.Text = "Version 3.71    Date:8/24/2016";
             this.label7.Click += new System.EventHandler(this.label7_Click);
             // 
             // labelLicenseNumber
